e1d857eac491aaeabe05f26af73c4b4214ffa61e
Juliana Michelsen <juju.michelsen@gmail.com>
2025-01-28 21:57:43 -0300

    Inclusão dos cruds de categorias, inicio do crud de livros

---
+++ exercicios_aula_3.py
@@ -0,0 +1,69 @@
+# Aula 03, Exercício 01
+# Entrar via teclado com o valor de uma temperatura em graus Celsius, calcular e exibir sua temperatura equivalente em Fahrenheit.
+# Fórmula: F = (C * 1.8) + 32
+# Obs.: ao imprimir o resultado usem o f-string para formatar a mensagem.
+temperatura_c = float(input("Digite a temperatura em graus Celsius: "))
+temperatura_f = (temperatura_c*1.8)+32
+print(f"A temperatura em Fahrenheite é: {temperatura_f}")
+
+# Aula 03, Exercício 02
+# Criar um programa para analisar a velocidade de um automóvel.
+# Solicitar via teclado os valores da aceleração (a em m/s2), velocidade inicial (v0 em m/s) e o tempo de percurso (t em s).
+# Calcular e exibir a velocidade final do automóvel em km/h. E exibir uma mensagem de acordo com a tabela abaixo:
+# V <= 40 -> Veículo muito lento
+# 40 < V <=60 -> Velocidade permitida
+# 60 < V <= 80 -> Velocidade de cruzeiro
+# 80 < V <= 120 -> Veículo rápido
+# V > 120 -> Veículo muito rápido
+# Fórmula para o cálculo da velocidade em km/h: velocidade final = (velocidade inicial + (a * t)) * 3.6
+# Obs.: ao imprimir o resultado usem o f-string para formatar a mensagem.
+
+aceleracao = (int(input("Digite a aceleração em m/s: ")))
+velocidade_inicial = (int(input("Digite a velocidade inicial em m/s: ")))
+tempo = (int(input("Digite o tempo de percurso inicial em s: ")))
+velocidade_final = (velocidade_inicial + (aceleracao * tempo)) * 3.6
+
+if velocidade_final <=40:
+    print(f"A velocidade final foi: {velocidade_final}")
+    print("Veículo muito lento.")
+elif velocidade_final > 40 and velocidade_final <= 60:
+    print(f"A velocidade final foi: {velocidade_final}")
+    print("Velocidade permitida.")
+elif velocidade_final > 60 and velocidade_final <= 80:
+    print(f"A velocidade final foi: {velocidade_final}")
+    print("Velocidade de cruzeiro.")
+elif velocidade_final > 80 and velocidade_final <= 120:
+    print(f"A velocidade final foi: {velocidade_final}")
+    print("Veículo rápido.")
+else:
+    print(f"A velocidade final foi: {velocidade_final}")
+    print("Veículo muito rápido.")
+
+# Aula 03, Exercício 03
+# Entrar via teclado com dois valores numéricos quaisquer. Após isso, exibir um seletor de opções (“menu”) com as seguintes opções:
+# Adição
+# Divisão
+# Multiplicação
+# Subtração
+# Sair do programa
+# Solicitar uma opção para o usuário e processar a respectiva operação.
+# Se o usuário digitar o número 5 mostrar uma mensagem de finalização e sair do programa.
+# Mostrar mensagem de erro se a opção escolhida não existir no menu e sair do programa.
+# Repare que a opção de número dois é de divisão. Se o denominador for zero o programa deverá 1) mostrar uma mensagem de erro e depois 2) sair do programa.
+
+num1 = int(input("Digite o primeiro número inteiro"))
+num2 = int(input("Digite o segundo número inteiro"))
+
+menu = """
+[Menu] Escolha uma das seguintes opções:
+'1 - Adição
+'2 - Multiplicação
+'3 - Subtração
+'0 - Sair do programa
+"""
+
+ 
+# Aula 03, Exercício 04
+
+# Entrar com dois números inteiros via teclado, onde o segundo deverá ser maior que o primeiro. Repetir a operação de leitura do segundo número enquanto a regra não for respeitada.
+ 
--- livraria.py.py
+++ livraria.py.py
@@ -1,10 +1,28 @@
 menu_principal = """
 [Menu Principal Escolha uma das seguintes opções:
-'1 - Categorias
-'2 - Editoras
-'3 - Autores
-'4 - Livros
-'0 - Sair do programa
+1 - Categorias
+2 - Editoras
+3 - Autores
+4 - Livros
+0 - Sair do programa
+"""
+
+menu_categorias = """
+[Categorias] Escolha uma das seguintes opções:
+1 - Listar todas as categorias
+2 - Adicionar uma nova categoria
+3 - Excluir categoria
+4 - Ver categoria por Id
+0 - Voltar ao menu anterior
+"""
+
+menu_editoras = """
+[Editoras] Escolha uma das seguintes opções:
+1 - Listar todas as editoras
+2 - Adicionar uma nova editora
+3 - Excluir editora
+4 - Ver editora por Id
+0 - Voltar ao menu anterior
 """
 
 menu_autores = """
@@ -12,46 +30,175 @@ menu_autores = """
 1 - Listar todos os autores
 2 - Adicionar novo autor
 3 - Excluir autor
-4 - Ver autoor por Id
+4 - Ver autor por Id
+0 - Voltar ao menu anterior
+"""
+
+menu_livros = """
+[Livros] Escolha uma das seguintes opções:
+1 - Listar todos os livros
+2 - Adicionar novo livro
+3 - Excluir livro
+4 - Ver livro por Id
 0 - Voltar ao menu anterior
 """
 
 tabela_autores = []
+tabela_categorias = []
+tabela_editoras = []
 
 while True:
     print(menu_principal)
+
+    print()
+
     opcao_principal = input('Digite a opção: ')
 
-    if opcao_principal == '3':
+    match opcao_principal:
+        case '0':
+            break
+        case '1':
+            while True:
+                print(menu_categorias)
+                opcao_categorias = input('Digite a opção: ')
+                match opcao_categorias:
+                    case '0':
+                        break  # interrompe o loop do while categorias
+                    case '1':
+                        if tabela_categorias == []:
+                            print("Nenhuma Categoria cadastrada.")
+                            input("Pressione <ENTER> para continuar.")
+                            continue
 
-        while True:
-            print(menu_autores)
-            opcao_autores = input('Digite a opção: ')
-            if opcao_autores == '0':    
-                break
-            if opcao_autores == "1":
-                if tabela_autores == []:
-                    print("Nenhum Autor cadastrado.")
-                else:
-                    print("Id | Nome | Estilo | Pais")
-                    for index, autor in enumerate(tabela_autores):
-                        print(f'{index} | {autor[0]} | {autor[1]} | {autor[2]}')
-            if opcao_autores == "2":
-                nome_autor = input("Digite o nome do autor: ")
-                estilo_autor = input("Digite o estilo do autor: ")
-                pais_autor = input("Digite o país do autor: ")
-                novo_autor = []
-                novo_autor.append(nome_autor) 
-                novo_autor.append(estilo_autor)
-                novo_autor.append(pais_autor)
-                tabela_autores.append(novo_autor)
-                print("Autor adicionado com sucesso!")
-            if opcao_autores =='3':
-                id = int(input('Digite o id do Autor a ser excluido'))
-                tabela_autores.pop(id)
-                print('Autor excluído com sucesso!')
+                        print('Id | Nome')
+                        for index, categoria in enumerate(tabela_categorias):
+                            print(f"{index} | {categoria['nome']}")
+                    case '2':
+                        nome_categoria = input('Digite o nome da categoria: ')
+                        nova_categoria = {
+                            'nome': nome_categoria
+                        }
+                        tabela_categorias.append(nova_categoria)
+                    case '3':
+                        if tabela_categorias == []:
+                            print("Nenhuma Categoria cadastrada.")
+                            input("Pressione <ENTER> para continuar.")
+                            continue
 
-    if opcao_principal == "0":
-        break
+                        id = int(input('Digite o ID da categoria a ser excluída: '))
+                        tabela_categorias.pop(id)
+                        print('Categoria excluída com sucesso!')
+                    case '4':
+                        if tabela_categorias == []:
+                            print("Nenhuma Categoria cadastrada.")
+                            input("Pressione <ENTER> para continuar.")
+                            continue
+
+                        id = int(input('Digite o ID da categoria para buscar: '))
+                        categoria = tabela_categorias[id]
+                        print('Id | Nome')
+                        print(f"{id} | {categoria['nome']}")
+                    case _:
+                        print('Opção inválida!')
+        case '2':
+            while True:
+                print(menu_editoras)
+                opcao_editoras = input('Digite a opção: ')
+                match opcao_editoras:
+                    case '0':    
+                        break
+                    case '1':
+                        if tabela_editoras == []:
+                            print("Nenhuma editora cadastrada.")
+                            input("Pressione <ENTER> para continuar.")
+                            continue
+
+                        print("Id | Nome | Endereço | Telefone")
+                        for index, editora in enumerate(tabela_editoras):
+                            print(f"{index} | {editora['nome']} | {editora['endereco']} | {editora['fone']}")
+                    case '2':
+                        nome_editora = input("Digite o nome da editora: ")
+                        endereco_editora = input("Digite o endereco da editora: ")
+                        fone_editora = input("Digite o telefone da editora: ")
+                        nova_editora = {
+                            'nome' : nome_editora,
+                            'endereco' : endereco_editora,
+                            'fone' : fone_editora,
+                        }
+                        tabela_editoras.append(nova_editora)
+                        print("Editora adicionada com sucesso!")
+                    case '3':
+                        if tabela_editoras == []:
+                            print("Nenhuma editora casastrada.")
+                            input("Pressione <ENTER> para continuar.")
+                            continue
+
+                        id = int(input('Digite o id da editpra a ser excluida: '))
+                        tabela_editoras.pop(id)
+                        print('Editora excluída com sucesso!')
+                    case '4':
+                        if tabela_editoras == []:
+                            print("Nenhuma editora cadastrada.")
+                            input("Pressione <ENTER> para continuar")
+                            continue
+                        
+                        id = int(input('Digite o ID da editora para buscar: '))
+                        editora = tabela_editoras[id]
+                        print("Id | Nome | Endereço | Telefone")
+                        print(f"{id} | {editora['nome']} | {editora['endereco']} | {editora['fone']}")
+
+        case '3':
+            while True:
+                print(menu_autores)
+                opcao_autores = input('Digite a opção: ')
+                match opcao_autores:    
+                    case '0':
+                        break
+                    case "1":
+                        if tabela_autores == []:
+                            print("Nenhum Autor cadastrado.")
+                            input("Pressione <ENTER> para continuar.")
+                            continue
+
+                        print("Id | Nome | Estilo | Pais | Bio")
+                        for index, autor in enumerate(tabela_autores):
+                            print(f'{index} | {autor['nome']} | {autor['estilo']} | {autor['pais']} | {autor['bio']}')
+                    case "2":
+                        nome_autor = input("Digite o nome do autor: ")
+                        estilo_autor = input("Digite o estilo do autor: ")
+                        pais_autor = input("Digite o país do autor: ")
+                        bio_autor = input("Digite a biografia do autor: ")
+                        novo_autor = {
+                            'nome': nome_autor,
+                            'estilo': estilo_autor,
+                            'pais': pais_autor,
+                            'bio' : bio_autor
+                        }
+                        tabela_autores.append(novo_autor)
+                        print("Autor adicionado com sucesso!")
+                    case '3':
+                        if tabela_autores == []:
+                            print("Nenhum autor cadastrado.")
+                            input("Pressione <ENTER> para continuar.")
+                            continue
+
+                        id = int(input('Digite o id do Autor a ser excluido'))
+                        tabela_autores.pop(id)
+                        print('Autor excluído com sucesso!')
+                    case '4':
+                        if tabela_autores == []:
+                            print("Nenhum autor cadastrado.")
+                            input("Pressione <ENTER> para continuar.")
+                            continue
+
+                        id = int(input('Digite o ID do autor para buscar: '))
+                        autor = tabela_autores[id]
+                        print("Id | Nome | Estilo | Pais | Bio")
+                        print(f'{index} | {autor['nome']} | {autor['estilo']} | {autor['pais']} | {autor['bio']}')
+                    case _:
+                        print('Opção Inválida!')                       
+        case _:
+            print('Opção Inválida!')
+
 
 print('Programa Encerrado!')
